f176631ae26281c10d1d158fa3456646a8f09f8a
Maurice Fiorani <mauricef97@gmail.com>
2020-02-24 21:45:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,13 +7,15 @@ class Triangle
   end
   
   def kind 
+    validation
     if (@side_1 == @side_2) && (@side_2 == @side_3)
       :equilateral
     elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
       :isosceles
     else (@side_1 != @side_2) || (@side_2 != @side_3) || (@side_1 != @side_3)
       :scalene
-    end  
+    end 
+    
   end
     
   def validation 
