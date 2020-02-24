5b6605686e9bc1cb36576a54cf618a6b883ec048
Maurice Fiorani <mauricef97@gmail.com>
2020-02-24 20:38:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,9 +14,15 @@ class Triangle
     elsif (@side_1 != @side_2) || (@side_2 != @side_3) || (@side_1 != @side_3)
       :scalene
     end
+    else
+      if (@side_1 <=0) || (@side_2 <= 0) || (@side_3 <=0)
+        raise TriangleError
+      end
   end
   
   class TriangleError < StandardError
+    
+  end  
     
   
 end
